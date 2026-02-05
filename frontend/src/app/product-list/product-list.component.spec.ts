9018bfff06b101d12708da479e69c10b0207b7e4
nicgen <nic@genty.dev>
2026-02-05 11:03:23 +0100

    fix(frontend): import NoopAnimationsModule to fix test failure

--- frontend/src/app/product-list/product-list.component.spec.ts
+++ frontend/src/app/product-list/product-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ProductListComponent } from './product-list.component';
 
@@ -9,7 +10,7 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductListComponent, HttpClientTestingModule]
+      imports: [ProductListComponent, HttpClientTestingModule, NoopAnimationsModule]
     })
       .compileComponents();
 
